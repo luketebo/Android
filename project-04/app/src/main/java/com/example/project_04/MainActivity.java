609d31dcbea21d24a98c2bf95049e95a7c846898
luketebo <mianqiangyang@gmail.com>
2022-10-18 15:04:57 +0800

    submit

---
+++ project-04/app/src/main/java/com/example/project_04/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.project_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 入口
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button_1);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 跳转到TextView演示界面
+                Intent intent = new Intent(MainActivity.this, TextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ project-04/app/src/main/java/com/example/project_04/TextActivity.java
@@ -0,0 +1,23 @@
+package com.example.project_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TextActivity extends AppCompatActivity {
+
+    private TextView Tv5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+        Tv5 = (TextView) findViewById(R.id.tv_5);
+        // 绘制中划线
+        Tv5.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        Tv5.getPaint().setAntiAlias(true); // 抗锯齿
+
+    }
+}
